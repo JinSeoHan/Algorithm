3ab2bf46faa7f96b18a51439bd0f1ba624293966
HanSeoJin <87526189+JinSeoHan@users.noreply.github.com>
2023-03-24 13:58:06 +0900

    [Bronze III] Title: 최소, 최대, Time: 376 ms, Memory: 155728 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,10 +1,3 @@
 a = map(int, input().split())
 arr = list(map(int, input().split()))
-max = arr[0]
-min = arr[0]
-for i in arr:
-    if i > max:
-        max = i
-    elif i < min:
-        min = i
-print("%d %d" %(min, max))
+print(f'{min(arr)} {max(arr)}')
