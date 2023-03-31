daca881fe1279fc5bb6a08175b645d1088a98e29
HanSeoJin <87526189+JinSeoHan@users.noreply.github.com>
2023-03-31 13:54:22 +0900

    [Bronze I] Title: 세로읽기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,9 @@
+arr = []
+mLength = 0
+for i in range(5):
+    arr += [list(input())]
+
+for _ in range(15):
+    for j in range(len(arr)):
+        if len(arr[j]) > 0: 
+            print(arr[j].pop(0), end='')
