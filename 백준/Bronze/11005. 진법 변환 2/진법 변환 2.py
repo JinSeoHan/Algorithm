9c0faadea1cc911d17efbc6369aa7d736c3964da
HanSeoJin <87526189+JinSeoHan@users.noreply.github.com>
2023-04-03 09:36:26 +0900

    [Bronze I] Title: 진법 변환 2, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -2,6 +2,6 @@ N, B = map(int, input().split())
 number = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 result = ''
 while N > 0:
-    N, mod = divmod(N, B)
-    result += number[mod]
+    result += number[N%B]
+    N //= B
 print(result[::-1])
