e7af2ab5bf2889fbfe26ef479acdd922b1180e78
HanSeoJin <87526189+JinSeoHan@users.noreply.github.com>
2023-04-10 00:05:26 +0900

    [Bronze II] Title: 소수 찾기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,32 +1,11 @@
-import sys
-input = sys.stdin.readline
-arr = [2]
+n = input()
+arr = list(map(int, input().split()))
 
-def isprim(num):
-    result = True
-    if num in arr:
-        return True
-    elif num == 1:
-        return False
-    else:
-        for i in arr:
-            if num % i == 0:
-                result = False
-                break
-        for i in range(arr[-1], num):
-            if num % i == 0:
-                result = False
-                break
-    if result:
-        arr.append(num)
-    return result
-
-N = int(input())
-number = list(map(int, input().split()))
 count = 0
-for e in range(1, 1001):
-    isprim(e)
-for i in number:
-    if i in arr:
-        count += 1
+for i in arr:
+    for j in range(2, i+1):
+        if i % j == 0:
+            if i == j:
+                count += 1
+            break
 print(count)
