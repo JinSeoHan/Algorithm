a88539f00131ba35e51e2e4e0a156dc9d3f8a38d
HanSeoJin <87526189+JinSeoHan@users.noreply.github.com>
2023-04-11 11:36:44 +0900

    [Bronze III] Title: 네 번째 점, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.py
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -3,15 +3,4 @@ for i in range(3):
     a, b = map(int, input().split())
     x.append(a)
     y.append(b)
-if x[0] == x[1]:
-    print(x[2], end=' ')
-elif x[0] == x[2]:
-    print(x[1], end=' ')
-else:
-    print(x[0], end=' ')
-if y[0] == y[1]:
-    print(y[2], end=' ')
-elif y[0] == y[2]:
-    print(y[1], end=' ')
-else:
-    print(y[0], end=' ')
+print(min(x, key=x.count), min(y, key=y.count))
