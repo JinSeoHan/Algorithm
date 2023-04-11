59d70bf082d13529c94421c0ceab0761b0196290
HanSeoJin <87526189+JinSeoHan@users.noreply.github.com>
2023-04-11 12:29:11 +0900

    [Bronze III] Title: 대지, Time: 92 ms, Memory: 35196 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.py
+++ 백준/Bronze/9063. 대지/대지.py
@@ -1,8 +1,9 @@
+import sys
 x = []
 y = []
 N = int(input())
 for i in range(N):
-    a, b = map(int, input().split())
+    a, b = map(int, sys.stdin.readline().split())
     x.append(a)
     y.append(b)
 print((max(x) - min(x)) * (max(y) - min(y)))
