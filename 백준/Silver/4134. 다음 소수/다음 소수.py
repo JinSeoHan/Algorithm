c582e6017850c50254791799825be2cc7549b063
HanSeoJin <87526189+JinSeoHan@users.noreply.github.com>
2023-05-11 20:38:38 +0900

    [Silver IV] Title: 다음 소수, Time: 1416 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+primeList = [2, 3]
+
+def isPrime(num : int) -> int:
+    root = int(num**(1/2))
+
+    if num < 2:
+        return False
+    elif num < 4:
+        return True
+
+    for i in range(2, root+1):
+        if num % i == 0:
+            return False
+        
+    return True
+
+def greaterPrime(a : int) -> int:
+    
+
+    num = a
+
+    while True:
+        if isPrime(num):
+            return num
+        num += 1
+
+n = int(input())
+for i in range(n):
+    a = int(input())
+    print(greaterPrime(a))
