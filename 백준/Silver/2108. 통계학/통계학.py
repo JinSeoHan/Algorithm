e641882ee3d84a931254383e9e0652a81341256b
HanSeoJin <87526189+JinSeoHan@users.noreply.github.com>
2023-05-17 23:20:49 +0900

    [Silver III] Title: 통계학, Time: 312 ms, Memory: 35324 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,56 +1,51 @@
-import math
 import sys
 input = sys.stdin.readline
-
-def realRound(num : float) -> int:
-    a = math.trunc(num)#정수부
-    b = num-a#소수부
-    if num > 0:
-        if b >= 0.5:
-            return a+1
-        else:
-            return a
-    elif num < 0:
-        if b <= -0.5:
-            return a-1
-        else:
-            return a
-    else:
-        return 0
-    
+ 
 n = int(input())
-lArr = list()
-dic = dict()
+lArr = [0] * 8001
+
+sum = 0
 for _ in range(n):
     num = int(input())
-    lArr.append(num)
-    if num in dic:
-        dic[num] += 1
-        isNever = False
-    else:
-        dic[num] = 0
+
+    #산술평균용
+    sum += num
+    #최빈값용
+    lArr[num] += 1
+
+inputList = list()
+maxList = list()
+maxValue = max(lArr)
+for i in range(-4000,4001):
+    if lArr[i] > 0:
+        #중앙값용
+        for j in range(lArr[i]):
+            inputList.append(i)
+        #최빈값용
+        if lArr[i] == maxValue:
+            maxList.append(i)
+
+dic = dict()
 
 #산술평균
-print(realRound(sum(lArr)/n))
+a = (sum/n)-(sum//n)#소수부분
+if a < 0:
+    if a <= -0.5:
+        print((sum//n)-1)
+    else:
+        print(sum//n)
+else:
+    if a >= 0.5:
+        print((sum//n)+1)
+    else:
+        print(sum//n)
+
 #중앙값
-sortedLArr = sorted(lArr)
-print(sortedLArr[n//2])
+print(inputList[n//2])
 #최빈값
-dic = sorted(dic.items(), key=lambda x : (x[1], x[0]))
-isPrint = False
-for i in range(len(dic)-1,-1,-1):
-    if dic[i][1] != dic[-1][1]:
-        if i+2 <= len(dic)-1:
-            print(dic[i+2][0])
-            isPrint =True
-            break
-        else:
-            print(dic[-1][0])
-            break
-    if i == 0:
-        if n == 1:
-            print(lArr[0])
-        else:
-            print(dic[1][0])
+if len(maxList) == 1:
+    print(maxList[0])
+else:
+    print(maxList[1])
 #범위
-print(sortedLArr[-1] - sortedLArr[0])
+print(inputList[-1] - inputList[0])
