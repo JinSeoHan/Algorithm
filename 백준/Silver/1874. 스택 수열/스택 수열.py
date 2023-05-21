1f20761414b50513af9867e6f6583f11d96b1bc9
HanSeoJin <87526189+JinSeoHan@users.noreply.github.com>
2023-05-21 16:09:25 +0900

    [Silver II] Title: 스택 수열, Time: 1616 ms, Memory: 31840 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+lArr = []
+pushCount = 1
+resultString = ''
+isNo = False
+for i in range(1, n+1):
+    num = int(input())
+
+    #들어온 값이 아직 push된적이 없는 수인 경우
+    if pushCount <= num:
+        while pushCount <= num:
+            lArr.append(pushCount)
+            pushCount += 1
+            resultString += '+'
+
+    if lArr[-1] == num:
+        lArr.pop()
+        resultString += '-'
+    else:
+        isNo = True
+        
+if isNo: 
+    print('NO')
+else:
+    for i in resultString:
+        print(i)
