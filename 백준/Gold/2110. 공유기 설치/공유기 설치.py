06cb83e681999ab30704e17b109384e0bec566d6
HanSeoJin <87526189+JinSeoHan@users.noreply.github.com>
2023-07-12 14:16:05 +0900

    [Gold IV] Title: 공유기 설치, Time: 896 ms, Memory: 41064 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n, c = map(int, input().split())
+l = [0] + [int(input()) for _ in range(n)]
+l.sort()
+lo, hi = 1, l[-1]
+while lo <= hi:
+    mid = (hi+lo)//2
+
+    stack = [l[1]]
+    for i in range(1, n+1):
+        if l[i] - stack[-1] >= mid:
+            stack.append(l[i])
+    if len(stack) >= c:
+        lo = mid+1
+    else:
+        hi = mid-1
+print(hi)
