e8be5eab4fda106efa9ffdb09024fef442e0ddcc
HanSeoJin <87526189+JinSeoHan@users.noreply.github.com>
2023-08-30 11:48:56 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.49 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,14 +1,7 @@
-zeroCnt = 0
-def deleteZero(s):
-    global zeroCnt
-    for i in s:
-        if i == '0': zeroCnt += 1
-    return s.replace('0', '')
 def solution(s):
-    convCnt = 0
+    convCnt, zeroCnt = 0, 0
     while s != '1':
-        s = deleteZero(s)
-        length = len(s)
-        s = str(bin(length)[2:])
+        zeroCnt += s.count('0')
+        s = str(bin(len(s.replace('0', '')))[2:])
         convCnt += 1
     return [convCnt, zeroCnt]
