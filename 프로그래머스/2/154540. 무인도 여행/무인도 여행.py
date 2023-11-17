81982b9395a21dd84c590f4abd07597e9750b5b0
HanSeoJin <87526189+JinSeoHan@users.noreply.github.com>
2023-11-17 18:39:03 +0900

    [level 2] Title: 무인도 여행, Time: 0.26 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -1,24 +1,23 @@
-import sys
-sys.setrecursionlimit(10**5)
+visited = None
 def isValid(i, j, maps):
     return 0 <= i < len(maps) and 0 <= j < len(maps[0])
-def dfs(i, j, maps, visited):
+def dfs(i, j, maps):
+    global visited
     cnt = 0
     visited[i][j] = True
     for position in [(0,1),(1,0),(0,-1),(-1,0)]:
         ni,nj = i + position[0], j + position[1]
         
         if not isValid(ni,nj, maps) or visited[ni][nj] or maps[ni][nj] == 'X': continue
-        cnt += int(dfs(ni,nj,maps, visited))
+        cnt += dfs(ni,nj,maps)
     return cnt + int(maps[i][j])
         
 def solution(maps):
+    global visited
     result = []
-    visited = [[False] * len(maps[0]) for _ in range(len(maps))]
+    visited = [[False] * len(maps[0]) for row in range(len(maps))]
     for i, row in enumerate(maps):
         for j, v in enumerate(row):
-            if visited[i][j] or v == 'X': 
-                continue
-            result.append(dfs(i, j, maps, visited))
-    
+            if visited[i][j] or v == 'X': continue
+            result.append(dfs(i, j, maps))
     return [-1] if len(result) == 0 else sorted(result)
