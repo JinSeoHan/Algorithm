9c5dc6e24a232c4745284efaf59e8b1e2dba5b67
HanSeoJin <87526189+JinSeoHan@users.noreply.github.com>
2023-12-14 20:47:58 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 204 ms, Memory: 112856 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,52 +1,28 @@
 import sys
+from itertools import permutations
 input = sys.stdin.readline
 
-def dfs(sum, nextNumIdx):
-    #종료조건
-    if nextNumIdx == n:
-        result.append(sum)
-        return
-
-    #연산을 수행함
-    for i in range(n-1):
-        #사용된 연산자 넘어감
-        if useCheck[i] == True:
-            continue
-
-        useCheck[i] = True
-        
-        dfs(operation(sum, nums[nextNumIdx], opers[i]), nextNumIdx + 1)
-        useCheck[i] = False
-
-def operation(a, b, oper):
-    if oper == 0:
-        return a + b
-    elif oper == 1:
-        return a - b
-    elif oper == 2:
-        return a * b
-    elif oper == 3:
-        if a < 0 and b > 0:
-            return -(-a // b)
-        return a // b
-        
-
 n = int(input())
-nums = list(map(int, input().split()))
-operNums = list(map(int, input().split()))
-opers = []
-for i in range(4):
-    if i == 0:
-        opers += [0] * operNums[i]
-    elif i == 1:
-        opers += [1] * operNums[i]
-    elif i == 2:
-        opers += [2] * operNums[i]
-    elif i == 3:
-        opers += [3] * operNums[i]
+nums = list(map(int, input().split()))#수
+opers = list(map(int, input().split()))#연산자별 개수
 
-useCheck = [False] * (n-1)#사용된 연산자 기록
-result = []
-dfs(nums[0], 1)
-print(max(result))
-print(min(result))
+maxNum, minNum = -10**15, 10**15
+def dfs(idx, s, plus, minus, mul, div):
+    global maxNum, minNum
+    if idx == n-1:
+        maxNum = max(maxNum, s)
+        minNum = min(minNum, s)
+        return
+    
+    if plus:
+        dfs(idx+1, s + nums[idx+1], plus-1, minus, mul, div)
+    if minus:
+        dfs(idx+1, s - nums[idx+1], plus, minus-1, mul, div)
+    if mul:
+        dfs(idx+1, s * nums[idx+1], plus, minus, mul-1, div)
+    if div:
+        dfs(idx+1, int(s / nums[idx+1]), plus, minus, mul, div-1)
+
+dfs(0, nums[0], opers[0], opers[1], opers[2], opers[3])
+print(maxNum)
+print(minNum)
