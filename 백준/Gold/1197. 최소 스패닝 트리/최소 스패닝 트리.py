c2ca0b2ee5d5378bab56adc45ccb3fbd1119f771
HanSeoJin <87526189+JinSeoHan@users.noreply.github.com>
2023-12-22 11:58:34 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 332 ms, Memory: 49332 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -2,37 +2,33 @@ import sys
 import heapq
 input = sys.stdin.readline
 
-def getParent(a):
-    if parent[a] == a:
-        return a
-    parent[a] = getParent(parent[a])
-    return parent[a]
-
-def unionParent(a, b):
-    a = getParent(a)
-    b = getParent(b)
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
-
-def isSameParent(a, b):
-    return getParent(a) == getParent(b)
-
-V, E = map(int, input().split())
+v, e = map(int, input().split())
 heap = []
+for i in range(e):
+    a, b, c = map(int, input().split())
+    heapq.heappush(heap, (c, a, b))
+
+def getParent(parent, a):
+    if parent[a] == a: return a
+    parent[a] = getParent(parent, parent[a])
+    return parent[a]
+def union(parent, a, b):
+    a = getParent(parent, a)
+    b = getParent(parent, b)
+    if a < b:
+        parent[b] = parent[a]
+    else:
+        parent[a] = parent[b]
+def isCycle(parent, a, b):
+    return getParent(parent, a) == getParent(parent, b)
+
+
+parent = [i for i in range(v+1)]
 result = 0
-parent = list(range(V+1))
-
-for i in range(E):
-    u, v, c = map(int, input().split())
-    heapq.heappush(heap, (c, u, v))
-
-while heap:
-    c, u, v = heapq.heappop(heap)
-
-    if not isSameParent(u, v):
-        unionParent(u, v)
-        result += c
-    
+for i in range(v-1):
+    c, a, b = heapq.heappop(heap)
+    while isCycle(parent, a, b):
+        c, a, b = heapq.heappop(heap)
+    union(parent, a, b)
+    result += c
 print(result)
