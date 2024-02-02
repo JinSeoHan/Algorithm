dcfc8260203f12302e2c0e8276ce8cd2fda64463
HanSeoJin <87526189+JinSeoHan@users.noreply.github.com>
2024-02-02 10:56:11 +0900

    [Gold III] Title: 파일 합치기, Time: 2028 ms, Memory: 119792 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.py
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.py
@@ -1,30 +1,32 @@
+'''
+힌트 : 행렬 곱셈 순서(113128) 문제와 비슷하게 접근
+
+dp[i][j]: i번파일부터 j번파일까지 합칠 때 드는 최소비용
+
+dp(s, e) = s~e파일합cost + dp(s, i) + dp(i+1, e) # (s <= i < e)
+'''
 import sys
 input = sys.stdin.readline
 
-def getMinCost(l : list) -> int:
-    cost = [[0]*(len(l)) for r in range(len(l))]
+def getMinimumJoinCost():
+    dp = [[0]*(k+1) for i in range(k+1)]
 
-    #범위 합을 빠르게 찾기 위한 누적합을 찾음
-    sums = [0]
-    for i in range(1, len(l)):
-        sums.append(sums[i-1] + l[i])
-
-    #s~e까지의 cost를 찾음
-    for diff in range(1, len(l)):#범위 크기
-        s = 1
-        e = s + diff
-
-        while e < len(l):
-            cost[s][e] = 10000000000
-            for i in range(s,e):
-                cost[s][e] = min(cost[s][e], cost[s][i]+cost[i+1][e] + sums[e] - sums[s-1])
-
-            s += 1
-            e = s + diff
-
-    return cost[1][len(l)-1]
+    for diff in range(1, k):
+        for s in range(1, k-diff+1):
+            e = s+diff
+            dp[s][e] = 1000000000000
+            for i in range(s, e):
+                dp[s][e] = min(dp[s][e], arr[e]-arr[s-1]+dp[s][i]+dp[i+1][e])
+    return dp[1][k]
 
 for _ in range(int(input())):
     k = int(input())
-    l = [0] + list(map(int, input().split()))
-    print(getMinCost(l))
+    nums = [0] + list(map(int,input().split()))
+
+    # 누적합
+    arr= []
+    arr.append(nums[0])
+    for num in nums[1:]:
+        arr.append(arr[-1]+num)
+
+    print(getMinimumJoinCost())
