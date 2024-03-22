0472a5a487ccff2fcac7609bf8ff1095cef8fa50
HanSeoJin <87526189+JinSeoHan@users.noreply.github.com>
2024-03-22 17:00:37 +0900

    [Silver I] Title: 오목, Time: 68 ms, Memory: 34136 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.py
+++ 백준/Silver/2615. 오목/오목.py
@@ -5,46 +5,21 @@ input = sys.stdin.readline
 board = [list(map(int, input().split())) for i in range(19)]
 visited = [[[False]*4 for col in range(19)] for row in range(19)]
 
+#범위 체크
 def valid(i, j):
     return 0 <= i < 19 and 0 <= j < 19
-#가로
-def dfs1(i, j, visited, target):
+
+# directions방향의 돌 개수를 찾음
+def dfs(i, j, visited, target, directions):
     visited[i][j] = True
     cnt = 0
-    for dir in [(0,1),(0,-1)]:
+    for dir in directions:
         ni, nj = i + dir[0], j + dir[1]
         if valid(ni, nj) and not visited[ni][nj] and target == board[ni][nj]:
-            cnt += dfs1(ni, nj, visited, target)
+            cnt += dfs(ni, nj, visited, target, directions)
     return cnt + 1
 
-#세로
-def dfs2(i, j, visited, target):
-    visited[i][j] = True
-    cnt = 0
-    for dir in [(1,0),(-1,0)]:
-        ni, nj = i + dir[0], j + dir[1]
-        if valid(ni, nj) and not visited[ni][nj] and target == board[ni][nj]:
-            cnt += dfs2(ni, nj, visited, target)
-    return cnt + 1
-#가로위
-def dfs3(i, j, visited, target):
-    visited[i][j] = True
-    cnt = 0
-    for dir in [(-1,1),(1,-1)]:
-        ni, nj = i + dir[0], j + dir[1]
-        if valid(ni, nj) and not visited[ni][nj] and target == board[ni][nj]:
-            cnt += dfs3(ni, nj, visited, target)
-    return cnt + 1
-#가로아래
-def dfs4(i, j, visited, target):
-    visited[i][j] = True
-    cnt = 0
-    for dir in [(-1,-1),(1,1)]:
-        ni, nj = i + dir[0], j + dir[1]
-        if valid(ni, nj) and not visited[ni][nj] and target == board[ni][nj]:
-            cnt += dfs4(ni, nj, visited, target)
-    return cnt + 1
-
+# 시작점을 찾음
 def getLeftEnd(i, j, rop, cop):
     target = board[i][j]
     while valid(i, j):
@@ -53,32 +28,20 @@ def getLeftEnd(i, j, rop, cop):
         j += cop
     return i-rop, j-cop
     
+#이동방향 정보 : 가로,세로,가로위,가로아래
+directionsInfo = [[(0,1),(0,-1)],[(1,0),(-1,0)],[(-1,1),(1,-1)],[(1,1),(-1,-1)]]
 def gameResult(si,sj):
     ri, rj = None, None
     target = board[si][sj]
 
-    #가로
-    visited = [[False]*19 for r in range(19)]
-    cnt = dfs1(si,sj, visited, target)
-    if cnt == 5:
-        ri, rj = getLeftEnd(si, sj, 0, -1)
-    #세로
-    visited = [[False]*19 for r in range(19)]
-    cnt = dfs2(si,sj, visited, target)
-    if cnt == 5:
-        ri, rj = getLeftEnd(si, sj, -1, 0)
-    #가로위
-    visited = [[False]*19 for r in range(19)]
-    cnt = dfs3(si,sj, visited, target)
-    if cnt == 5:
-        ri, rj = getLeftEnd(si, sj, 1, -1)
-    #가로아래
-    visited = [[False]*19 for r in range(19)]
-    cnt = dfs4(si,sj, visited, target)
-    if cnt == 5:
-        ri, rj = getLeftEnd(si, sj, -1, -1)
+    # 이동방향으로 이동
+    for dirInfo in directionsInfo:
+        visited = [[False]*19 for r in range(19)]
+        cnt = dfs(si,sj,visited,target, dirInfo)
+        if cnt == 5:
+            rop, cop = dirInfo[1][0], dirInfo[1][1]
+            ri, rj = getLeftEnd(si, sj, rop, cop)
     return ri, rj
-
 
 for i in range(19):
     for j in range(19):
